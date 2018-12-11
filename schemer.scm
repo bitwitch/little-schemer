7b66533078754a3cb59c6bea542662a4eb14f554
bitwitch <shawwian@gmail.com>
2018-12-11 01:03:38 -0500

    initial commit

---
+++ schemer.scm
@@ -0,0 +1,50 @@
+(define atom?
+   (lambda (x)
+     (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)  
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      ((eq? a (car lat)) #t)
+      (else (member? a (cdr lat))))))
+
+(define rember
+  (lambda (a lat)
+    (cond 
+      ((null? lat) '())
+      ((eq? a (car lat)) (cdr lat))
+      (else (cons (car lat) (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (lol)
+    (cond
+      ((null? lol) '())
+      (else (cons (car (car lol)) 
+                  (firsts (cdr lol)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond 
+      ((null? lat) '())
+      ((eq? (car lat) old) 
+            (cons (car lat) (cons new (cdr lat))))
+      (else (cons (car lat) (insertR new old (cdr lat)))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old)
+            (cons new lat))
+      (else (cons (car lat) (insertL new old (cdr lat)))))))
+
+
+
